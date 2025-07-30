a8842fccb16daee681826d53dd597482ff84f048
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-07-31 00:29:42 +0630

    add: prefilled rejected form datas

--- user/views/applications/employee_rejected.php
+++ user/views/applications/employee_rejected.php
@@ -1,17 +1,17 @@
 <?php
 require_once __DIR__ . '/helpers/drop_datas.php';
-if(!isset($_SESSION['user_id'])){
-    header("Location: ".BASE_URL."?vr=applications");
+if (!isset($_SESSION['user_id'])) {
+    header("Location: " . BASE_URL . "?vr=applications");
     exit;
 }
 $userModel = new UserModel();
 $userModel->user_id = $_SESSION['user_id'];
 $reject_app = $userModel->get_registered_application();
-if($reject_app['status'] != 'Rejected'){
-    header("Location: ".BASE_URL."?vr=applications");
+if ($reject_app['status'] != 'Rejected') {
+    header("Location: " . BASE_URL . "?vr=applications");
     exit;
 }
-
+$images = json_decode($reject_app['images']);
 ?>
 <style>
     main h1 {
@@ -19,7 +19,7 @@ if($reject_app['status'] != 'Rejected'){
     }
 
     form {
-        width: 60%;
+        width: 70%;
         margin: auto;
         box-sizing: border-box;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
@@ -38,7 +38,7 @@ if($reject_app['status'] != 'Rejected'){
         margin: auto;
     }
 
-    div{
+    main div {
         box-sizing: border-box;
     }
 
@@ -56,7 +56,6 @@ if($reject_app['status'] != 'Rejected'){
         padding: 8px 10px;
         border: 1px solid #ccc;
         border-radius: 6px;
-        /* font-size: 1em; */
     }
 
     form input[type="submit"] {
@@ -131,7 +130,7 @@ if($reject_app['status'] != 'Rejected'){
         border-radius: 12px;
         object-fit: cover;
         box-shadow: 0 2px 8px rgb(0 0 0 / 0.07);
-        display: none;
+        /* display: none; */
     }
 
     .status {
@@ -209,20 +208,21 @@ if($reject_app['status'] != 'Rejected'){
             flex-direction: column;
         }
 
-        #nrc{
+        #nrc {
             flex-wrap: wrap;
         }
 
-        #nrc select{
+        #nrc select {
             width: 25%;
             /* font-size: 0.3rem; */
         }
-        #nrc input{
+
+        #nrc input {
             width: 100%;
         }
     }
 </style>
-<script src="<?= BASE_URL . '/static/js/load_nrc_datas.js' ?>"></script>
+<!-- <script src=""></script> -->
 <main>
     <h1>မြန်မာအလုပ်သမား စာရင်းသွင်းခြင်း</h1>
     <form action="<?= BASE_URL ?>/submits/submit_application.php" method="POST" id="reg-form"
@@ -231,13 +231,11 @@ if($reject_app['status'] != 'Rejected'){
             <div style="grid-column: span 2; display: flex; gap: 20px; align-items: center;">
                 <div style="flex: 1;">
                     <label for="pic">
-                    <img id="preview" src="/labor_application/<?= $reject_app['picture'] ?>"
-                        alt="Image Preview"
-                        style="width: 200px; height: 200px; border: 1px solid #ccc; padding: 5px; object-fit: cover;">
+                        <img id="preview" src="/labor_application/<?= $reject_app['picture'] ?>" alt="Image Preview"
+                            style="width: 200px; height: 200px; border: 1px solid #ccc; padding: 5px; object-fit: cover;">
                     </label>
                 </div>
                 <div style="flex: 1;">
-                    <!-- <label for="pic">Choose Image:</label> -->
                     <input type="file" name="picture" id="pic" data-att="Profile" accept="image/*" hidden>
                 </div>
             </div>
@@ -250,16 +248,25 @@ if($reject_app['status'] != 'Rejected'){
             <div>
                 <label for="nrc">မှတ်ပုံတင်အမှတ် (NRC):</label>
                 <div id="nrc">
-                    <select name="nrc_code" id="nrc-code" required>
+                    <?php
+                    if (preg_match('/^(\d+)\/([^\(]+)\(([^)]+)\)(\d+)$/u', $reject_app['nrc'], $matches)) {
+                        $code = $matches[1];      // 12
+                        $region = $matches[2];    // ခလဖ
+                        $type = $matches[3];      // N
+                        $nrc_no = $matches[4];    // 102102
+                    }
+                    ?>
+                    <select name="nrc_code" aria-valuemax="" id="nrc-code" required>
                     </select>
                     <p>/</p>
                     <select name="region" id="region" required>
                     </select>
                     <select name="nrc_type" id="nrc-type" required>
-                        <option value="နိုင်">နိုင်</option>
-                        <option value="ဧည့်">ဧည့်</option>
+                        <option value="နိုင်" <?= $type == 'နိုင်' ? 'selected' : '' ?>>နိုင်</option>
+                        <option value="ဧည့်" <?= $type == 'ဧည့်' ? 'selected' : '' ?>>ဧည့်</option>
                     </select>
-                    <input type="text" name="nrc_no" placeholder="မှတ်ပုံတင်နံပါတ်" id="nrc-no" maxlength="6">
+                    <input type="text" name="nrc_no" placeholder="မှတ်ပုံတင်နံပါတ်" value="<?= $nrc_no ?>" id="nrc-no"
+                        maxlength="6">
                 </div>
             </div>
 
@@ -267,7 +274,7 @@ if($reject_app['status'] != 'Rejected'){
                 <label for="township">မြို့နယ် (Township):</label>
                 <select name="township">
                     <?php foreach ($myanmarTownships as $township): ?>
-                        <option value="<?= htmlspecialchars($township) ?>"><?= htmlspecialchars($township) ?></option>
+                        <option value="<?= htmlspecialchars($township) ?>" <?= $reject_app['township'] == $township ? 'selected' : '' ?>><?= htmlspecialchars($township) ?></option>
                     <?php endforeach; ?>
                 </select>
 
@@ -275,24 +282,24 @@ if($reject_app['status'] != 'Rejected'){
 
             <div>
                 <label for="email">Email (Email):</label>
-                <input type="email" id="email" name="email">
+                <input type="email" id="email" name="email" value="<?= $reject_app['email'] ?>">
             </div>
 
             <div>
                 <label for="phone">Phone Number :</label>
-                <input type="text" id="phone" name="phone" maxlength="14" required>
+                <input type="text" id="phone" name="phone" value="<?= $reject_app['phone'] ?>" maxlength="14" required>
             </div>
 
             <div>
                 <label for="birth_date">မွေးသက္ကရာဇ် (Birth Date):</label>
-                <input type="date" id="birth_date" name="birth_date" required>
+                <input type="date" id="birth_date" name="birth_date" value="<?= $reject_app['birth_date'] ?>" required>
             </div>
 
             <div>
                 <label for="gender">ကျား / မ (Gender):</label>
                 <select id="gender" name="gender" required>
-                    <option value="Male">ကျား (Male)</option>
-                    <option value="Female">မ (Female)</option>
+                    <option value="Male" <?= $reject_app['gender'] == 'Male' ? 'selected' : '' ?>>ကျား (Male)</option>
+                    <option value="Female" <?= $reject_app['gender'] == 'Female' ? 'selected' : '' ?>>မ (Female)</option>
                 </select>
             </div>
 
@@ -300,7 +307,7 @@ if($reject_app['status'] != 'Rejected'){
                 <label for="religion">ကိုးကွယ်သည့်ဘာသာ (Religion):</label>
                 <select name="religion" id="religion" required>
                     <?php foreach ($religions as $religion): ?>
-                        <option value="<?= htmlspecialchars($religion) ?>"><?= htmlspecialchars($religion) ?></option>
+                        <option value="<?= htmlspecialchars($religion) ?>" <?= $reject_app['religion'] == $religion ? 'selected' : '' ?>><?= htmlspecialchars($religion) ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
@@ -309,28 +316,37 @@ if($reject_app['status'] != 'Rejected'){
                 <label for="edu_level">ပညာအရည်အချင်း (Education Level):</label>
                 <select name="edu_level" id="edu_level" required>
                     <?php foreach ($educationLevels as $level): ?>
-                        <option value="<?= htmlspecialchars($level) ?>"><?= htmlspecialchars($level) ?></option>
+                        <option value="<?= htmlspecialchars($level) ?>" <?= $reject_app['edu_level'] == $level ? 'selected' : '' ?>><?= htmlspecialchars($level) ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
 
             <div>
                 <label for="stable_address">အမြဲတမ်းနေရပ်လိပ်စာ (Stable Address):</label>
-                <input type="text" id="stable_address" name="stable_address" required>
+                <input type="text" id="stable_address" name="stable_address" value="<?= $reject_app['stable_address']  ?>" required>
             </div>
         </div>
         <div class="attachments">
             <div class="attachment">
-                <label for="file1">မှတ်ပုံတင်</label>
-                <input type="file" id="file1" name="images[0]" accept="image/*" data-att="Nrc"
+                <?php
+                    $nrc_imgs = json_decode($images->nrc, true);
+                ?>
+                <label for="file1">မှတ်ပုံတင် (Front)</label>
+                <input type="file" id="file1" name="nrc[0]" accept="image/*" data-att="Nrc"
                     aria-describedby="desc1" />
-                <img id="preview1" class="preview" alt="Preview of Image File 1" />
+                <img id="preview1" class="preview" src="<?= '/labor_application/'.$nrc_imgs[0] ?>" alt="Preview of Image File 1" />
             </div>
             <div class="attachment">
-                <label for="file2">Certificate</label>
-                <input type="file" id="file2" name="images[1]" accept="image/*" data-att="Certificate"
+                <label for="file2">မှတ်ပုံတင် (Back)</label>
+                <input type="file" id="file2" name="nrc[1]" accept="image/*" data-att="Nrc"
                     aria-describedby="desc2" />
-                <img id="preview2" class="preview" alt="Preview of Image File 2" />
+                <img id="preview2" src="<?= '/labor_application/'.$nrc_imgs[1] ?>" class="preview" alt="Preview of Image File 2" />
+            </div>
+            <div class="attachment">
+                <label for="file3">Certificate</label>
+                <input type="file" id="file3" name="images[1]" accept="image/*" data-att="Certificate"
+                    aria-describedby="desc3" />
+                <img id="preview3" class="preview" src="<?= '/labor_application/'.$images->certificate ?>" alt="Preview of Image File 3" />
             </div>
         </div>
         <input type="submit" value="စာရင်းသွင်းမည် (Register)">
@@ -390,7 +406,7 @@ if($reject_app['status'] != 'Rejected'){
 
             reader.onload = function (e) {
                 preview.src = e.target.result;
-                preview.style.display = 'block';
+                // preview.style.display = 'block';
             };
 
             reader.readAsDataURL(file);
@@ -406,7 +422,7 @@ if($reject_app['status'] != 'Rejected'){
                 const reader = new FileReader();
                 reader.onload = (e) => {
                     preview.src = e.target.result;
-                    preview.style.display = 'block';
+                    // preview.style.display = 'block';
                 };
                 reader.readAsDataURL(file);
             } else {
@@ -419,6 +435,99 @@ if($reject_app['status'] != 'Rejected'){
     document.getElementById("reg-form") && ['file1', 'file2'].forEach((inputId, idx) => {
         setupPreview(inputId, 'preview' + (idx + 1));
     });
+    document.addEventListener('DOMContentLoaded', () => {
+        let nrcData = [];
+        let paired_datas = {};
+        const nrc_code = document.getElementById("nrc-code");
+        const regions = document.getElementById('region');
+        if (nrc_code != 'null' && regions) {
+            const fetchNRCData = async () => {
+                try {
+                    const response = await fetch('/labor_application/user/static/js/nrc.json');
+                    if (response.ok) {
+                        nrcData = await response.json();
+                        console.log('Stored NRC data:', nrcData);
+                        populateNRCOptions(nrcData.data);
+                    } else {
+                        console.error('Failed to fetch NRC data');
+                    }
+                } catch (err) {
+                    console.error('Error:', err);
+                }
+            };
 
+            const populateNRCOptions = (datas) => {
+                generate_code_state_pair(datas);
+                const burmese_number = {
+                    0: '၀',
+                    1: '၁',
+                    2: '၂',
+                    3: '၃',
+                    4: '၄',
+                    5: '၅',
+                    6: '၆',
+                    7: '၇',
+                    8: '၈',
+                    9: '၉',
+                    10: '၁၀',
+                    11: '၁၁',
+                    12: '၁၂',
+                    13: '၁၃',
+                    14: '၁၄'
+                };
+                for (let i = 1; i <= 14; i++) {
+                    nrc_code.innerHTML += `<option value="${burmese_number[i]}" ${burmese_number[i] === '<?= $code ?>' ? "selected" : ""}>${burmese_number[i]}</option>`;
+                }
+                updateRegionsDropdown(1);
+            };
+
+            nrc_code.addEventListener('change', (e) => {
+                let code = e.target.value;
+                const english_number = {
+                    '၀': 0,
+                    '၁': 1,
+                    '၂': 2,
+                    '၃': 3,
+                    '၄': 4,
+                    '၅': 5,
+                    '၆': 6,
+                    '၇': 7,
+                    '၈': 8,
+                    '၉': 9,
+                    '၁၀': 10,
+                    '၁၁': 11,
+                    '၁၂': 12,
+                    '၁၃': 13,
+                    '၁၄': 14
+                };
+                code = english_number[code];
+                console.log('Selected NRC code:', code);
+                updateRegionsDropdown(code);
+            });
+
+            function generate_code_state_pair(datas) {
+                datas.forEach(data => {
+                    if (paired_datas[data.nrc_code]) {
+                        paired_datas[data.nrc_code].push(data.name_mm);
+                    } else {
+                        paired_datas[data.nrc_code] = [data.name_mm];
+                    }
+                });
+            }
+
+            function updateRegionsDropdown(code) {
+                regions.innerHTML = '';
+                if (paired_datas[code]) {
+                    paired_datas[code].forEach(region => {
+                        regions.innerHTML += `<option value="${region}">${region}</option>`;
+                    });
+                } else {
+                    regions.innerHTML = `<option value="">No regions found</option>`;
+                }
+            }
+
+            fetchNRCData();
+        }
+    })
 
 </script>
