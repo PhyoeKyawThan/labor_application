7f2918df6899d093f0faa633d6d5dd88564d9b52
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-08-04 17:05:54 +0630

    add: sql

---
+++ sql/lrdb(3).sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 04, 2025 at 12:35 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lrdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `title`, `body`) VALUES
+(1, 'About Us', '<div align=\"center\"><font size=\"6\">asdfasdfasfasdfasdfs</font></div>');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `nrc` varchar(100) DEFAULT NULL,
+  `picture` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `registration_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `township` varchar(100) NOT NULL,
+  `serial_number` varchar(100) DEFAULT NULL,
+  `birth_date` date NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `religion` varchar(100) NOT NULL,
+  `edu_level` varchar(100) NOT NULL,
+  `stable_address` varchar(255) NOT NULL,
+  `images` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `status` enum('Approved','Pending','Rejected') NOT NULL DEFAULT 'Pending',
+  `message` text DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `is_resubmit` tinyint(1) NOT NULL DEFAULT 0,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `applications`
+--
+
+INSERT INTO `applications` (`id`, `name`, `nrc`, `picture`, `phone`, `email`, `registration_date`, `township`, `serial_number`, `birth_date`, `gender`, `religion`, `edu_level`, `stable_address`, `images`, `status`, `message`, `user_id`, `is_resubmit`, `updated_at`) VALUES
+(9, 'updated name', '၁၄/ခလဖ(နိုင်)122222', '/static/uploads/picture/f6e736c1941dd9eb8b007d49837cb0dc.png', '09666775646', 'dom@gmail.com', '2025-07-30 23:33:52', 'ဟင်္သာတ', '001', '2007-07-03', 'Female', 'ဗုဒ္ဓဘာသာ', 'မူလတန်းအောင်', 'asdf', '{\"nrc\":\"[\\\"static\\\\\\/uploads\\\\\\/nrc\\\\\\/688a692fbe074_photo_2025-05-28_18-42-06.jpg\\\",\\\"static\\\\\\/uploads\\\\\\/nrc\\\\\\/688a692fbe105_photo_2025-05-28_18-42-13.jpg\\\"]\",\"certificate\":\"\\/static\\/uploads\\/certificate\\/6bf0b2b5c4bc56ce2d8ef71805ac7ea9.jpeg\"}', 'Approved', 'We are checking ur details\r\nadsf', 11, 0, '2025-07-30 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `google_map` text NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `google_map`, `phone`, `email`, `address`) VALUES
+(1, 'htsdf', 'asdf', 'sadf', 'asdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_msg`
+--
+
+CREATE TABLE `contact_msg` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `reply_msg` text DEFAULT NULL,
+  `replied` enum('Yes','No') NOT NULL DEFAULT 'No',
+  `submitted_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_req_form`
+--
+
+CREATE TABLE `employee_req_form` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `department_address` text DEFAULT NULL,
+  `po_box_number` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `report_receiver` text DEFAULT NULL,
+  `submitted_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_req_form`
+--
+
+INSERT INTO `employee_req_form` (`id`, `user_id`, `name`, `position`, `department_address`, `po_box_number`, `phone`, `report_receiver`, `submitted_at`) VALUES
+(1, 14, 'asdf', 'asdf', 'asdf', 'asdf', '0989893498', 'asdfasdf', '2025-08-03 19:06:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serial_numbers`
+--
+
+CREATE TABLE `serial_numbers` (
+  `id` int(11) NOT NULL,
+  `form_id` int(11) NOT NULL,
+  `serial_number` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `serial_numbers`
+--
+
+INSERT INTO `serial_numbers` (`id`, `form_id`, `serial_number`) VALUES
+(1, 1, '001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` enum('employer','employee') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `type`, `created_at`) VALUES
+(11, 'Tester', 'tester@gmail.com', '$2y$10$gWO81JF5oOAVZIiajAmKze6lNp6gJE5s9dHHRiNsLge0ikPmIWiSG', 'employee', '2025-07-30 18:55:17'),
+(12, 'kyawkyaw', 'kyaw@gmail.com', '$2y$10$Ld0efm2h25ti9VQgp.l1WeLZgGzz6ozf/zktrG5sxMdmoVOKYPmqC', 'employee', '2025-07-31 01:36:54'),
+(13, 'hlahla', 'hla@gmail.com', '$2y$10$DhQdYZOASApjA8H4LkMCJORrVz3GJkLmVh6R8E1KOQ.N8SW0GD/U.', 'employee', '2025-07-31 01:41:04'),
+(14, 'employer', 'employer@gmail.com', '$2y$10$4/deCK34mjTAgadwt0xnT.vbpzrIbwe7UDkD2/SsJanGJLmkN8BuG', 'employer', '2025-07-31 17:02:42');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `serial_number` (`serial_number`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_msg`
+--
+ALTER TABLE `contact_msg`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee_req_form`
+--
+ALTER TABLE `employee_req_form`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `serial_numbers`
+--
+ALTER TABLE `serial_numbers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `form_id` (`form_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact_msg`
+--
+ALTER TABLE `contact_msg`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee_req_form`
+--
+ALTER TABLE `employee_req_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `serial_numbers`
+--
+ALTER TABLE `serial_numbers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `applications`
+--
+ALTER TABLE `applications`
+  ADD CONSTRAINT `applications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `employee_req_form`
+--
+ALTER TABLE `employee_req_form`
+  ADD CONSTRAINT `employee_req_form_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `serial_numbers`
+--
+ALTER TABLE `serial_numbers`
+  ADD CONSTRAINT `serial_numbers_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `employee_req_form` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
